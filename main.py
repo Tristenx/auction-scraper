55493c22655e590d30da362a6956061aa3a27204
Tristenx <1604tristenarron@gmail.com>
2025-06-17 16:24:54 +0100

    small change to whats displayed in terminal

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ from price_check import PriceCheck
 
 auction_content = AuctionContent()
 price_check = PriceCheck(auction_content)
-print(price_check.read_price_data())
+print(price_check.bids)
+print(price_check.price_after_tax)
+print(price_check.prices)
