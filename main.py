981deae1a554899920c8d728474fa330547f2909
Tristenx <1604tristenarron@gmail.com>
2025-06-21 19:33:52 +0100

    exception for no data in print

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ price_check.calculate_prices()
 print("ITEM | AUCTION_PRICE | RETAIL_PRICE")
 
 for i in range(len(auction_content.item_descriptions)):
-    if price_check.price_after_tax[i] < price_check.prices[i]:
+    if price_check.prices[i] != "NO DATA" and float(price_check.price_after_tax[i]) < float(price_check.prices[i]):
         print(
             f"{auction_content.item_descriptions[i]} | {price_check.price_after_tax[i]} | {price_check.prices[i]}")
