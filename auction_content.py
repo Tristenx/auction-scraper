7de66a83a32d5246cec67debac6d159caebae520
Tristenx <1604tristenarron@gmail.com>
2025-07-06 00:48:43 +0100

    refactored auction content class, still need to reimplement multiple pages

--- auction_content.py
+++ auction_content.py
@@ -10,53 +10,18 @@ from webdriver_manager.chrome import ChromeDriverManager
 class AuctionContent:
     def __init__(self):
         self.url = "https://www.johnpyeauctions.co.uk/"
-        self.text = []
-        self.lot_numbers = []
-        self.item_descriptions = []
-        self.current_bids = []
-        self.time_remaining = []
-
-    def read_content(self):
-        with open(file="search_results.txt", mode="r") as file:
-            self.text = file.read()
-
-        lines = self.text.split("\n")
-        for i in range(len(lines)):
-            words = lines[i].split(" ")
-            if words[0] == "Lot":
-                self.lot_numbers.append(words[1])
-                self.item_descriptions.append(lines[i+1])
-                self.current_bids.append(lines[i+4])
-                self.time_remaining.append(lines[i+5])
-
-    def read_page(self):
-        WebDriverWait(self.driver, 5).until(EC.presence_of_element_located(
-            (By.CLASS_NAME, "row")))
-
-        item = self.driver.find_element(
-            By.XPATH, "/html/body/main/div/div[2]/div[2]/div[3]/div")
-
-        with open(file="search_results.txt", mode="a") as file:
-            file.write(item.text)
+        self.search_query = input("Search: ")
+        self.lots = []
 
     def multiple_pages(self) -> bool:
         """Checks if there are multiple pages."""
-        if EC.presence_of_element_located((By.CLASS_NAME, "pagination")):
+        if WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "pagination"))):
             return True
-        else:
-            return False
+        return False
 
-    def scrape_data(self):
-        item = input("SEARCH: ")
-
-        with open(file="search_results.txt", mode="w") as file:
-            file.write("")
-
-        # ---------------------------- LOAD WEBSITE ------------------------------- #
-        service = Service(ChromeDriverManager().install())
-        self.driver = webdriver.Chrome(service=service)
-
-        self.driver.get("https://www.johnpyeauctions.co.uk/")
+    def load_website(self):
+        """Loads the website and accepts cookies."""
+        self.driver.get(self.url)
 
         WebDriverWait(self.driver, 5).until(
             EC.element_to_be_clickable(
@@ -66,26 +31,37 @@ class AuctionContent:
             By.CLASS_NAME, "cc-nb-okagree")
         accept_button.click()
 
-        # ---------------------------- SEARCH FOR ITEM ------------------------------- #
+    def enter_search_query(self):
+        """Types search query in the search bar and hits enter."""
         WebDriverWait(self.driver, 5).until(
             EC.presence_of_element_located((By.ID, "FullTextQuery1")))
 
-        input_element = self.driver.find_element(By.ID, "FullTextQuery1")
-        input_element.clear()
-        input_element.send_keys(item + Keys.ENTER)
+        search_bar = self.driver.find_element(By.ID, "FullTextQuery1")
+        search_bar.clear()
+        search_bar.send_keys(self.search_query + Keys.ENTER)
 
-        # ---------------------------- READ PAGES ------------------------------- #
-        if self.multiple_pages():
-            page_navigator = self.driver.find_element(
-                By.CLASS_NAME, "pagination")
-            num_pages = int(page_navigator.text[-3])
+    def read_page(self) -> str:
+        """Searches for the element which contains the lots then returns its text as a string."""
+        WebDriverWait(self.driver, 5).until(EC.presence_of_element_located(
+            (By.CLASS_NAME, "row")))
 
-            for i in range(num_pages):
-                self.read_page()
-                next_page = self.driver.find_element(
-                    By.XPATH, '//ul[@class="pagination"]//a[text()="»"]')
-                next_page.click()
-        else:
-            self.read_page()
+        target_element = self.driver.find_element(
+            By.XPATH, "/html/body/main/div/div[2]/div[2]/div[3]/div")
+        return target_element.text
+
+    def get_lots(self) -> str:
+        """Searches the website for the search query and returns all of the lots as a string."""
+        self.service = Service(ChromeDriverManager().install())
+        self.driver = webdriver.Chrome(service=self.service)
+
+        self.load_website()
+        self.enter_search_query()
+
+        lots = self.read_page()
 
         self.driver.quit()
+        return lots
+
+
+auction_content = AuctionContent()
+print(auction_content.get_lots())
